e9fecaf28136e25d4ba177a44d36ee1b5f4c7a2e
andersonar12 <aromerocangri.14@gmail.com>
2022-02-03 14:26:05 -0400

    se cambio icono principal del Header

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,14 +1,11 @@
 <template>
-  <div
-    class='bus-font'
-    :style="windowSize.x < 600 ? 'margin-bottom: 55px;': 'margin-bottom: 64px;'"
-  >
+  <div class="bus-font" :style="windowSize.x < 600 ? 'margin-bottom: 55px;' : 'margin-bottom: 64px;'">
     <v-navigation-drawer
       clipped
       fixed
-      v-model='drawer'
+      v-model="drawer"
       app
-      :drawer='drawer'
+      :drawer="drawer"
       dark
       disable-resize-watcher
       temporary
@@ -17,16 +14,11 @@
     >
       <v-container>
         <v-list>
-          <v-list-group
-            v-for='item in sidePanelItems'
-            :value='item.active'
-            :key='item.title'
-            class='noTextDecoration'
-          >
-            <v-list-tile slot='item' @click="$router.push({path: item.path})">
+          <v-list-group v-for="item in sidePanelItems" :value="item.active" :key="item.title" class="noTextDecoration">
+            <v-list-tile slot="item" @click="$router.push({ path: item.path })">
               <v-list-tile-action>
                 <template v-if="item.icon.indexOf('static') > 0">
-                  <img class="drawerBtnLogo" :src="item.icon">
+                  <img class="drawerBtnLogo" :src="item.icon" />
                 </template>
                 <template v-else>
                   <v-icon>{{ item.icon }}</v-icon>
@@ -39,12 +31,16 @@
                 </v-list-tile-title>
               </v-list-tile-content>
 
-              <v-list-tile-action v-if='item.items'>
+              <v-list-tile-action v-if="item.items">
                 <v-icon>keyboard_arrow_down</v-icon>
               </v-list-tile-action>
             </v-list-tile>
 
-            <v-list-tile v-for='subItem in item.items' :key='subItem.title' @click="subItemAction(subItem, item.action)">
+            <v-list-tile
+              v-for="subItem in item.items"
+              :key="subItem.title"
+              @click="subItemAction(subItem, item.action)"
+            >
               <v-list-tile-content>
                 <v-list-tile-title>
                   <span>{{ subItem.title }}</span>
@@ -57,38 +53,37 @@
     </v-navigation-drawer>
 
     <v-toolbar
-      :fixed='true'
-      :class='{ header_transparent: !logo, header_solid:logo}'
-      class='elevation-5 px-0' style="z-index: 6; overflow: hidden"
+      :fixed="true"
+      :class="{ header_transparent: !logo, header_solid: logo }"
+      class="elevation-5 px-0"
+      style="z-index: 6; overflow: hidden"
     >
-      <v-toolbar-side-icon class="displayNoneTiny white--text" @click.stop='drawer = !drawer' :dark='!logo' id="toolbarDrawer"></v-toolbar-side-icon>
+      <v-toolbar-side-icon
+        class="displayNoneTiny white--text"
+        @click.stop="drawer = !drawer"
+        :dark="!logo"
+        id="toolbarDrawer"
+      ></v-toolbar-side-icon>
       <v-layout row class="px-2 mx-0">
         <v-flex xs2 sm2 style="overflow: hidden">
           <v-toolbar-items>
             <v-btn
-              flat :dark='!logo'
+              flat
+              :dark="!logo"
               small
               class="white--text"
-              :style="windowSize.x <= 960 ? 'min-width: 0px; width: 100%': ''"
+              :style="windowSize.x <= 960 ? 'min-width: 0px; width: 100%' : ''"
               @click="openDialog = true"
             >
-              <img src='/static/imgs/logos/Contacto.png' class="headerBtnLogo"/>
+              <img src="/static/imgs/logos/Contacto.png" class="headerBtnLogo" />
               <span class="hidden-sm-and-down header-text white--text capitalize" v-lang.contact></span>
             </v-btn>
           </v-toolbar-items>
         </v-flex>
 
-        <v-flex xs2 sm1 v-if="
-          windowSize.x > 600 &&
-          sidePanelItems.find((item) => item.title === 'Cybermonday')
-        ">
+        <v-flex xs2 sm1 v-if="windowSize.x > 600 && sidePanelItems.find(item => item.title === 'Cybermonday')">
           <v-toolbar-items>
-            <v-btn
-              flat :dark='!logo'
-              small
-              class="white--text"
-              @click="$router.push({ name: 'gifcard' })"
-            >
+            <v-btn flat :dark="!logo" small class="white--text" @click="$router.push({ name: 'gifcard' })">
               <span class="cybermonday">CYBERMONDAY</span>
             </v-btn>
           </v-toolbar-items>
@@ -100,8 +95,8 @@
           <v-layout row>
             <v-flex md3 xs12 class="logo-vb">
               <v-toolbar-items>
-                <v-btn flat :dark='!logo' small @click="$router.push({path: '/'})">
-                  <img v-if='logo' src='/static/imgs/logos/Logo2.png' alt class='principalLogo'>
+                <v-btn flat :dark="!logo" small @click="$router.push({ path: '/' })">
+                  <img v-if="logo" src="/static/imgs/logos/boletos-paraguay-logo.svg" alt class="principalLogo" />
                 </v-btn>
               </v-toolbar-items>
             </v-flex>
@@ -115,7 +110,13 @@
             <template v-if="windowSize.x <= 600">
               <v-flex xs6 offset-xs6>
                 <v-toolbar-items>
-                  <v-btn flat :dark='!logo' @click.stop='drawer = !drawer' class='white--text' style="width: 50px; min-width: 0px">
+                  <v-btn
+                    flat
+                    :dark="!logo"
+                    @click.stop="drawer = !drawer"
+                    class="white--text"
+                    style="width: 50px; min-width: 0px"
+                  >
                     <v-icon>more_vert</v-icon>
                   </v-btn>
                 </v-toolbar-items>
@@ -126,22 +127,22 @@
               <v-flex xs3 sm6 md9 offset-xs3 offset-sm0 offset-md4>
                 <v-toolbar-items>
                   <template v-if="Object.keys(user.usuario).length <= 0">
-                    <v-btn flat :dark='!logo' small class="white--text" @click="$router.push({path: '/login'})">
-                      <img src='/static/imgs/logos/Quienes-somos.png' class="headerBtnLogo"/>
+                    <v-btn flat :dark="!logo" small class="white--text" @click="$router.push({ path: '/login' })">
+                      <img src="/static/imgs/logos/Quienes-somos.png" class="headerBtnLogo" />
                       <span class="hidden-sm-and-down header-text capitalize" v-lang.login></span>
                     </v-btn>
                   </template>
 
                   <template v-else>
                     <v-menu bottom offset-y style="display: flex; height: 100%; align-items: center;">
-                      <v-btn flat :dark='!logo' slot='activator' class='white--text'>
-                        <img src='/static/imgs/logos/Quienes-somos.png' class="headerBtnLogo"/>
+                      <v-btn flat :dark="!logo" slot="activator" class="white--text">
+                        <img src="/static/imgs/logos/Quienes-somos.png" class="headerBtnLogo" />
                         <span class="hidden-sm-and-down header-text capitalize" v-lang.my_account></span>
                         <v-icon>arrow_drop_down</v-icon>
                       </v-btn>
 
                       <v-list>
-                        <v-list-tile @click="$router.push({name: 'my_profile'})">
+                        <v-list-tile @click="$router.push({ name: 'my_profile' })">
                           <v-list-tile-title v-lang.my_profile></v-list-tile-title>
                         </v-list-tile>
 
@@ -168,13 +169,13 @@
     </v-toolbar>
 
     <v-snackbar
-      class='mt-5'
-      :timeout='2000'
-      :top='true'
-      :right='true'
-      color='primary'
-      multi-line='multi-line'
-      v-model='snackbarLanguage'
+      class="mt-5"
+      :timeout="2000"
+      :top="true"
+      :right="true"
+      color="primary"
+      multi-line="multi-line"
+      v-model="snackbarLanguage"
     >
       <span v-lang.language_msg></span>
     </v-snackbar>
@@ -200,7 +201,7 @@ export default {
     ContactHubspot
   },
 
-  data () {
+  data() {
     return {
       openDialog: false,
       windowSize: { x: window.innerWidth, y: window.innerHeight },
@@ -212,7 +213,7 @@ export default {
     ...mapGetters({
       user: ['userData']
     }),
-    sidePanelItems () {
+    sidePanelItems() {
       let profileRoutes = {}
       if (Object.keys(this.user.usuario).length <= 0) {
         profileRoutes = {
@@ -323,45 +324,45 @@ export default {
       ]
     }
   },
-  mounted () {
+  mounted() {
     this.changeLanguage(this.$store.state.language, true)
     this.$nextTick(() => {
       window.addEventListener('resize', this.onResizeHeader)
     })
   },
-  beforeDestroy () {
+  beforeDestroy() {
     window.removeEventListener('resize', this.onResizeHeader)
   },
   methods: {
-    subItemAction (item, cb) {
+    subItemAction(item, cb) {
       if (item.path) {
-        this.$router.push({path: item.path})
+        this.$router.push({ path: item.path })
         return
       }
       if (cb) {
         cb(item)
       }
     },
-    signOut () {
+    signOut() {
       this.$store.dispatch('DELETE_USER')
-      this.$router.push({path: '/'})
+      this.$router.push({ path: '/' })
     },
-    onResizeHeader () {
+    onResizeHeader() {
       this.windowSize = { x: window.innerWidth, y: window.innerHeight }
     },
-    accountAction (item) {
+    accountAction(item) {
       if (item.type === 'sign_out') {
         this.signOut()
       }
     },
-    languageAction (item) {
+    languageAction(item) {
       if (item.title === 'Español') {
         this.changeLanguage('es')
       } else {
         this.changeLanguage('en')
       }
     },
-    changeLanguage (lang, firstLoad) {
+    changeLanguage(lang, firstLoad) {
       if (!firstLoad) {
         this.snackbarLanguage = true
       }
@@ -379,7 +380,7 @@ export default {
 
 .cybermonday {
   padding: 5px 10px;
-  background-color: #7296F7;
+  background-color: #7296f7;
   border-radius: 20px;
 }
 
